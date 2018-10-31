a1391c3229c962c60da0e118c3fa9637538b75ae
Clint Kunz <clintkunz@gmail.com>
2018-10-31 17:19:40 -0600

    netlify authorization widget

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,6 +7,7 @@ const SideBar = (props) => {
     return (
         <div className="side-bar">
             <h1>Lambda<br/>Notes</h1>
+            <div data-netlify-identity-menu></div>
             <button><NavLink to="/" className="button">View Your Notes</NavLink></button>
             <button><NavLink to="/create-note" className="button">+ Create New Note</NavLink></button>
             <button><CSVLink data={props.notes} className="button">Download Notes</CSVLink></button>
